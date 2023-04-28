0e43c14409d2110eafe5a4c63703ea1f05c219f8
Andreas Hinderberger <info@gertex.us>
2023-04-28 09:25:33 -0500

    Initial commit

--- src/VinData.php
+++ src/VinData.php
@@ -1,1 +1,59 @@
 <?php
+
+namespace gertexllc\yii2\vindecoder;
+
+/**
+ * VinData is a container for decoded vehicle VIN data.
+ */
+class VinData
+{
+    /**
+     * @var string The make of the vehicle.
+     */
+    public $make;
+
+    /**
+     * @var string The model of the vehicle.
+     */
+    public $model;
+
+    /**
+     * @var string The year of the vehicle.
+     */
+    public $year;
+
+    /**
+     * @var string The trim level of the vehicle.
+     */
+    public $trim;
+
+    /**
+     * @var string The body type of the vehicle.
+     */
+    public $bodyType;
+
+    /**
+     * @var string The engine type of the vehicle.
+     */
+    public $engineType;
+
+    /**
+     * @var string The drive type of the vehicle.
+     */
+    public $driveType;
+
+    /**
+     * @var string The primary fuel type of the vehicle.
+     */
+    public $fuelType;
+
+    /**
+     * @var string The country where the vehicle was manufactured.
+     */
+    public $plantCountry;
+
+    /**
+     * @var string The state where the vehicle was manufactured.
+     */
+    public $plantState;
+}
--- src/VinDecoder.php
+++ src/VinDecoder.php
@@ -1,1 +1,142 @@
 <?php
+
+namespace gertexllc\yii2\vindecoder;
+
+/**
+ * VinDecoder is a Yii2 component for decoding vehicle VINs.
+ */
+class VinDecoder extends \yii\base\Component
+{
+    /**
+     * @var string The URL of the NHTSA VIN decoder API.
+     */
+    const API_URL = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValuesExtended/';
+
+    /**
+     * @var VinData The decoded VIN data.
+     */
+    private $_vinData;
+
+    /**
+     * Decodes the specified VIN and returns the decoded data as a VinData object.
+     *
+     * @param string $vin The VIN to decode.
+     * @return VinData The decoded VIN data.
+     * @throws \Exception If the VIN is not in a valid format, the check digit is incorrect, or the API call fails.
+     */
+    public function decode($vin)
+    {
+        // Validate the VIN format and check digit.
+        $checkDigit = $this->calculateCheckDigit($vin);
+        if ($vin[8] != $checkDigit) {
+            throw new \Exception("Invalid VIN check digit: $checkDigit");
+        }
+
+        // Make the API request.
+        $url = self::API_URL . "?format=json&data=$vin&modelyear=&make=&manufacturer=&plant=";
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+
+        // Check for errors.
+        if (isset($data['Results'][0]['ErrorReport']['ErrorCode'])) {
+            throw new \Exception("NHTSA API error: {$data['Results'][0]['ErrorReport']['Description']}");
+        }
+
+        // Load the response into the VinData object.
+        $this->_vinData = new VinData();
+        $this->loadResponse($data, $this->_vinData);
+
+        return $this->_vinData;
+    }
+
+    /**
+     * Calculates the check digit for the specified VIN.
+     *
+     * @param string $vin The VIN to calculate the check digit for.
+     * @return string The calculated check digit.
+     * @throws \Exception If the VIN is not in a valid format or contains an invalid character.
+     */
+    private function calculateCheckDigit($vin)
+    {
+        $transliterationTable = [
+            'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8, 'I' => 9, 'J' => 1,
+            'K' => 2, 'L' => 3, 'M' => 4, 'N' => 5, 'O' => 6, 'P' => 7, 'Q' => 8, 'R' => 9, 'S' => 2, 'T' => 3,
+            'U' => 4, 'V' => 5, 'W' => 6, 'X' => 7, 'Y' => 8, 'Z' => 9
+        ];
+
+        $weights = [
+            8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2
+        ];
+
+        $vin = strtoupper($vin);
+        $checkDigit = 0;
+
+        if (!preg_match('/^[A-Z0-9]{17}$/', $vin)) {
+            throw new \Exception('Invalid VIN format');
+        }
+
+        for ($i = 0; $i < 17; $i++) {
+            $char = $vin[$i];
+            if (isset($transliterationTable[$char])) {
+                $char = $transliterationTable[$char];
+            }
+            $checkDigit += $weights[$i] * $char;
+        }
+
+        $checkDigit %= 11;
+        if ($checkDigit == 10) {
+            $checkDigit = 'X';
+        }
+
+        return strval($checkDigit);
+    }
+
+    /**
+     * Loads the decoded VIN data from the specified API response into the specified VinData object.
+     *
+     * @param array $data The decoded JSON data from the NHTSA VIN decoder API.
+     * @param VinData $vinData The VinData object to load the data into.
+     */
+    private function loadResponse($data, $vinData)
+    {
+        foreach ($data['Results'][0] as $key => $value) {
+            switch ($key) {
+                case 'Make':
+                    $vinData->make = $value;
+                    break;
+                case 'Model':
+                    $vinData->model = $value;
+                    break;
+                case 'ModelYear':
+                    $vinData->year = $value;
+                    break;
+                case 'Trim':
+                    $vinData->trim = $value;
+                    break;
+                case 'BodyClass':
+                    $vinData->bodyType = $value;
+                    break;
+                case 'EngineType':
+                    $vinData->engineType = $value;
+                    break;
+                case 'DriveType':
+                    $vinData->driveType = $value;
+                    break;
+                case 'FuelTypePrimary':
+                    $vinData->fuelType = $value;
+                    break;
+                case 'PlantCountry':
+                    $vinData->plantCountry = $value;
+                    break;
+                case 'PlantState':
+                    $vinData->plantState = $value;
+                    break;
+                default:
+                    // Ignore unknown fields.
+                    break;
+            }
+        }
+    }
+
+}
+
